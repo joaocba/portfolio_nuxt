8e2b26c7d9c5dc4f1607ac1cbc10d88aebae4728
Joao B. <93290335+joaocba@users.noreply.github.com>
2024-12-11 15:21:22 +0000

    Data fetching with Api

---
+++ app/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container mx-auto max-w-2xl">
+        <header class="flex justify-between items-center mt-5">
+            <div>
+                <NuxtLink
+                    to="/"
+                    class="text-xl font-semibold p-2 hover:bg-gray-200"
+                    >Joao C</NuxtLink
+                >
+            </div>
+            <Menu />
+        </header>
+
+        <main class="p-2 mt-10">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    titleTemplate: "%s - Joao C",
+    link: [
+        {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+        },
+        {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+            crossorigin: "",
+        },
+    ],
+});
+</script>
+
+<style>
+body {
+    font-family: "Roboto", sans-serif;
+}
+</style>
---
+++ app/pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>This page is about me</div>
+    <!--     <div>
+        <button @click="enableCustomLayout">Update layout</button>
+    </div> -->
+</template>
+
+<script setup>
+useHead({
+    title: "About",
+});
+</script>
