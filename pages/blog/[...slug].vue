42eb263d9b4e5875570d72f6865e7e7be8cb4fa5
Joao B. <93290335+joaocba@users.noreply.github.com>
2024-12-11 21:13:36 +0000

    Refactored folder structure to match nuxt 3

---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,9 @@
+<template>
+    <article class="prose dark:prose-dark">
+        <ContentDoc />
+    </article>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    darkmode: 'class',
+    plugins: [
+        require('@tailwindcss/typography'),
+      ],
+}
